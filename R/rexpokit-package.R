9e1523199349057c3bd14d6dc2ea965322b3c482
nmatzke <matzke@berkeley.edu>
2019-07-01 12:40:31 +1200

    removed lapack.f and all subroutine z*
    subroutine z* from EXPOKIT weren't used for anything (they are for exponentiating complex number arrays), removing to work on CRAN LTO issues

--- R/rexpokit-package.R
+++ R/rexpokit-package.R
@@ -3,8 +3,8 @@
 #' \tabular{ll}{
 #' Package: \tab rexpokit\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.26.6.2\cr
-#' Date: \tab 2019-06-27\cr
+#' Version: \tab 0.26.6.3\cr
+#' Date: \tab 2019-07-01\cr
 #' License: \tab GPL (>= 2)\cr
 #' LazyLoad: \tab yes\cr
 #' }
